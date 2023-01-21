600c52d4a71c9f93e0edc534f8c67470f6539536
0412shsh <0412corn@naver.com>
2023-01-21 16:48:27 +0900

    [add] 숫자 비교하기

---
+++ src/lev1/Compare.java
@@ -0,0 +1,11 @@
+package lev1;
+
+public class Compare {
+    int solution(int num1, int num2) {
+        int answer = -1;
+        if(num1 == num2) {
+            answer = 1;
+        }
+        return answer;
+    }
+}
