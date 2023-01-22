57fd31f32e5682c7b81565b47713bcef2a280805
0412shsh <0412corn@naver.com>
2023-01-22 18:56:41 +0900

    [add] 각도기

---
+++ src/lev1/Protractor.java
@@ -0,0 +1,11 @@
+package lev1;
+
+public class Protractor {
+    int solution(int angle) {
+        int answer = 4;
+        if(angle<90) return 1;
+        if(angle==90) return 2;
+        if(angle<180) return 3;
+        return answer;
+    }
+}
